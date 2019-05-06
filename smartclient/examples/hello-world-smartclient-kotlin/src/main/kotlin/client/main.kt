929b5b87b4c2337d234594fe06841b415c10493b
dvekeman <dieter.vekeman@gmail.com>
2019-05-06 11:00:46 +0200

    Minor doc update

--- smartclient/examples/hello-world-smartclient-kotlin/src/main/kotlin/client/main.kt
+++ smartclient/examples/hello-world-smartclient-kotlin/src/main/kotlin/client/main.kt
@@ -14,6 +14,11 @@ fun main() {
     // Typed DynamicForm: the example below shows setting properties in two ways
     // - Using (untyped) dynamic JavaScript code ({ autoFocus: true, ... })
     // - Using typed properties on the DynamicForm object (`form.autoFocus = true`)
+    // Important, not the difference between
+    // title: nameTitle
+    // defaultValue: "$defaultValue"
+    //
+    // Both will insert the `nameTitle` variable! The latter will only work with string constants though
     val form = isc.DynamicForm.create(js(
         """{
 autoFocus: true,
@@ -22,14 +27,14 @@ items: [
     {
         type: "text",
         name: "you",
-        title: "$nameTitle",
+        title: nameTitle,
         selectOnFocus: true,
         wrapTitle: false,
         defaultValue: "$defaultValue"
     }, 
     {
         type: "button",
-        title: "$buttonTitle",
+        title: buttonTitle,
         width: 80,
         startRow: false,
         icon: "message.png",
@@ -42,3 +47,23 @@ items: [
     form.numCols = 3
 
 }
+
+// Note: Alternative way to deal with records:
+
+//val dsTitleField: dynamic = object {}
+//dsTitleField["name"] = "dsTitle"
+//dsTitleField["title"] = "Select a DataSource"
+//grid.setFields(arrayOf(dsTitleField))
+//
+//val dsAnimals: dynamic = object {}
+//dsAnimals["dsTitle"] = "Animals"
+//dsAnimals["dsName"] = "animals"
+//
+//val dsOffice: dynamic = object {}
+//dsOffice["dsTitle"] = "Office Supplies"
+//dsOffice["dsName"] = "supplyItem"
+//
+//val dsEmployees: dynamic = object {}
+//dsEmployees["dsTitle"] = "Employees"
+//dsEmployees["dsName"] = "employees"
+//grid.setData(arrayOf(dsAnimals, dsOffice, dsEmployees))
